58258fdc522be00693af983ecee7f01432b6a656
DKendle <dina.kendle@gmail.com>
2021-09-29 20:30:57 -0400

    gets form rendering

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import { createStore } from 'redux';
 
-// add imports and code
 
+
+
+const store = createStore(manageUsers, composeWithDevTools());
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
+  <Provider store={store} >
+     <App />
+  </Provider>
   ,
   document.getElementById('root')
 );
